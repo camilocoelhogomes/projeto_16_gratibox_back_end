4bda3d450420973e3b6e200ff80100d332fadeac
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-11-22 10:38:40 -0300

    setting products validations to be an array

--- src/controller/newSignature/validateNewSignature.js
+++ src/controller/newSignature/validateNewSignature.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const validateNewSignature = ({ signatureObject }) => Joi.object({
   userDeliveryDateId: Joi.number().required(),
-  userProductOptionsId: Joi.number().required(),
+  userProductOptionsId: Joi.array().min(1).required(),
   userAddress: Joi.required(),
 }).validate(signatureObject).error;
 
